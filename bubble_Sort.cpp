c64b06db0285c56cbb73ca7413d159e5b49082aa
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-05 15:30:55 +0700

    Penambahan keterangan informasi pada code

--- bubble_Sort.cpp
+++ bubble_Sort.cpp
@@ -9,19 +9,23 @@ void input()
 { //Prosedur untuk input data ke dalam array
     while (true)
     {
-        cout << "Masukkan banyaknya elemen pada array: ";
-        cin >> n;
-        if (n <= 20)
-            break;
+        cout << "Masukkan banyaknya elemen pada array: "; // Output ke layar
+        cin >> n;                                         // Input dari pengguna
+        if (n <= 20)                                      // Jika n kurang dari atau sama dengan 20, maka keluar dari loop
+            break;                                        // Keluar dari loop
         else
-            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
+        {                                                           // Jika n lebih dari 20, maka tampilkan pesan error dan minta input ulang
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n"; // Output ke layar jika n lebih dari 20
+        }
     }
+    cout << endl;
     cout << "\n===================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "===================" << endl;
+
     for (int i = 0; i < n; i++)
-    {
-        cout << "Data ke-" << (i + 1) << ": ";
-        cin >> arr[i];
+    {                                           // Looping dengan i dimulai dari 0 hingga n-1
+        cout << "Data ke-" << (i + 1) << ": ";  // Output ke layar
+        cin >> arr[i];                          // Input dari pengguna
     }
 }
