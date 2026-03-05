ec2ae2339a63635fcfdcc1f789284261c8b1c0e5
Cmry2047 <ahmadfadhilardiansyah7@gmail.com>
2026-03-05 15:18:08 +0700

    Menambahkan Prosedur untuk input data ke dalam array

--- bubble_Sort.cpp
+++ bubble_Sort.cpp
@@ -4,3 +4,24 @@ using namespace std;
 
 int arr[20]; //Deklarasi variable global array a dengan ukuran 20
 int n; //Deklarasi variable global n untuk menyimpan banyaknya elemen pada array
+
+void input()
+{ //Prosedur untuk input data ke dalam array
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else
+            cout << "\nArray dapat memiliki maksimal 20 elemen.\n";
+    }
+    cout << "\n===================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "===================" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
